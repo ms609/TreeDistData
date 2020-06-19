5374ea048e3b3f7103704c3531e98cceaf0ef6bf
Martin Smith <martins@gmail.com>
2020-06-19 16:03:47 +0100

    Update names

--- man-roxygen/methodAbbrevs.R
+++ man-roxygen/methodAbbrevs.R
@@ -1,16 +1,16 @@
 #' @details
 #' # Methods tested
 #'
-#' - `dpi`: Different Phylogenetic Information (Smith, forthcoming)
+#' - `pid`: Different Phylogenetic Information (Smith, forthcoming)
 #' - `msid`: Matching Split Information Distance (Smith, forthcoming)
 #' - `cid`: Clustering Information Distance (Smith, forthcoming)
 #' - `qd`: Quartet divergence (Smith, 2019)
-#' - `nts`: Nye _et al._ tree similarity (Nye _et al._ 2006)
-#' - `ja2`, `ja4`: Jaccard-Robinson-Foulds distances with _k_ = 2, 4,
+#' - `nye`: Nye _et al._ tree similarity (Nye _et al._ 2006)
+#' - `jnc2`, `jnc4`: Jaccard-Robinson-Foulds distances with _k_ = 2, 4,
 #'            conflicting pairings prohobited
-#' - `jna2`, `jna4`: Jaccard-Robinson-Foulds distances with _k_ = 2, 4,
+#' - `joc2`, `jco4`: Jaccard-Robinson-Foulds distances with _k_ = 2, 4,
 #'            conflicting pairings permitted
-#' - `msd`: Matching Split Distance (Bogdanowicz & Giaro, 2012)
+#' - `ms`: Matching Split Distance (Bogdanowicz & Giaro 2012)
 #' - `mast`: Size of Maximum Agreement Subtree (Valiente 2009)
 #' - `masti`: Information content of Maximum Agreement Subtree
 #' - `nni_l`, `nni_t`, `nni_u`: Lower bound, tight upper bound, and upper bound
@@ -18,8 +18,8 @@
 #' - `spr`: Approximate SPR distance
 #' - `tbr_l`, `tbr_u`: Lower and upper bound for tree bisection and reconnection
 #'          (TBR) distance
-#' - `rf`: Robinson-Foulds distance (Robinson & Foulds 1985)
-#' - `rfi`: Information-corrected Robinson-Foulds distance (Smith, forthcoming)
+#' - `rf`: Robinson-Foulds distance (Robinson & Foulds 1981)
+#' - `icrf`: Information-corrected Robinson-Foulds distance (Smith, forthcoming)
 #' - `path`: Path distance (Steel & Penny 1993), unnormalized
 #'
 #' @references
