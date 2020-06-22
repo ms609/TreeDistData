3a8ae6c7bbfcede9c692d4195ba20f09950ded87
Martin Smith <martins@gmail.com>
2020-06-22 08:40:33 +0100

    Format & completeness

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -6,24 +6,32 @@ test_that("Data dimensions are correct", {
 
   LengthWithout <- function(...) nMetrics - length(list(...))
 
-  data("sevenTipDistances", package='TreeDistData')
+  data("sevenTipDistances", package = 'TreeDistData')
   expect_equal(LengthWithout('mafi'), length(sevenTipDistances))
   expect_equal(c(945L, 945L), dim(sevenTipDistances[[1]]))
 
-  data("distanceDistribution25", package='TreeDistData')
+  data("distanceDistribution25", package = 'TreeDistData')
   expect_equal(c(LengthWithout('mafi'), 10000L),
                dim(distanceDistribution25))
   expect_equal(dim(distanceDistribution50), dim(distanceDistribution25))
 
   noMafiNnit <- tdMethods[!tdMethods %in% c('mafi', 'nni_t')]
 
-  data("randomTreeDistances", package='TreeDistData')
+  data("randomTreeDistances", package = 'TreeDistData')
   nLeafMeasurements <- 197L
   expect_true(all(noMafiNnit %in% dimnames(randomTreeDistances)[[1]]))
   expect_equal(c(LengthWithout('mafi', 'nni_t'), 13L, nLeafMeasurements),
                dim(randomTreeDistances))
 
-  expect_true(all(noMafiNnit %in% names(bullseyeDistances[[1]])))
-  expect_true(all(noMafiNnit %in% dimnames(bullseyeMorphScores[[1]])[[2]]))
+  lapply(bullseyeDistances, function (x) {
+    expect_true(all(noMafiNnit %in% names(x)))
+  })
+  lapply(bullseyeMorphScores, function (x) {
+    expect_true(all(noMafiNnit %in% dimnames(x)[[2]]))
+  })
+  lapply(bullMoDiScores, function (x) {
+    expect_true(all(noMafiNnit %in% dimnames(x)[[2]]))
+  })
+
   #TODO update other datasets for all 20 metrics?
 })
