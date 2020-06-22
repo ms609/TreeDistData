a323be382ccb6d27f11083a004383938bde954aa
Martin Smith <martins@gmail.com>
2020-06-22 13:57:33 +0100

    nni_t; distance not sim

--- man-roxygen/methodsTested.R
+++ man-roxygen/methodsTested.R
@@ -5,7 +5,7 @@
 #' - `msid`: Matching Split Information Distance (Smith, forthcoming)
 #' - `cid`: Clustering Information Distance (Smith, forthcoming)
 #' - `qd`: Quartet divergence (Smith, 2019)
-#' - `nye`: Nye _et al._ tree similarity (Nye _et al._ 2006)
+#' - `nye`: Nye _et al._ tree distance (Nye _et al._ 2006)
 #' - `jnc2`, `jnc4`: Jaccard-Robinson-Foulds distances with _k_ = 2, 4,
 #' conflicting pairings prohibited ('no-conflict')
 #' - `joc2`, `jco4`: Jaccard-Robinson-Foulds distances with _k_ = 2, 4,
@@ -13,11 +13,13 @@
 #' - `ms`: Matching Split Distance (Bogdanowicz & Giaro 2012)
 #' - `mast`: Size of Maximum Agreement Subtree (Valiente 2009)
 #' - `masti`: Information content of Maximum Agreement Subtree
-#' - `nni_l`, `nni_t`, `nni_u`: Lower bound, tight upper bound, and upper bound
-#'         for  nearest-neighbour interchange distance (Li _et al._ 1996)
+#' - `nni_l`,`r if (<%= nni_t %>) " ``nni_t``,"` `nni_u`: Lower
+#' `r ifelse(<%= nni_t %>, "bound, tight upper bound, and upper bound", "and upper bounds")`
+#' for nearest-neighbour interchange distance (Li _et al._ 1996)
 #' - `spr`: Approximate SPR distance
 #' - `tbr_l`, `tbr_u`: Lower and upper bound for tree bisection and reconnection
-#'          (TBR) distance
+#' (TBR) distance, calculated using
+#' [\pkg{TBRDist}](https://ms609.github.io/TBRDist/)
 #' - `rf`: Robinson-Foulds distance (Robinson & Foulds 1981)
 #' - `icrf`: Information-corrected Robinson-Foulds distance (Smith, forthcoming)
-#' - `path`: Path distance (Steel & Penny 1993), unnormalized
+#' - `path`: Path distance (Steel & Penny 1993)
