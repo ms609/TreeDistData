5256a2a1cec8ce975e8c9fc972d56ab7b92362a4
Martin Smith <martins@gmail.com>
2021-04-21 14:08:47 +0100

    root = TRUE

--- data-raw/LinTests.R
+++ data-raw/LinTests.R
@@ -1,5 +1,4 @@
-library('ape')
-library('TreeTools')
+library('TreeTools', quietly = TRUE, warn.conflicts = FALSE)
 library('TreeDist')
 library('TreeDistData')
 
@@ -14,7 +13,7 @@ message("Running tests on ", nTrees, ' ', nTip, "-leaf trees; ",
         replicates, " replicates.")
 
 LinTestOneSet <- function (nTip, k, nTrees) {
-  skeleton <- RandomTree(seq_len(k))
+  skeleton <- RandomTree(seq_len(k), root = TRUE)
   structure(lapply(seq_len(nTrees), function (XX) {
     tr <- skeleton
     for (i in k + seq_len(nTip - k))
