02624a87b5a534643e2a2d1bf4606011e0b706f3
Martin Smith <martins@gmail.com>
2021-05-03 10:55:27 +0100

    +es; kc
    Note change in nye/qd calculation sequence

--- data-raw/bullseyeMorphDistort.R
+++ data-raw/bullseyeMorphDistort.R
@@ -34,70 +34,72 @@ CacheFile <- function (..., tmpDir = FALSE) {
 # Create subdirectories:
 if (!dir.exists(CacheFile())) dir.create(CacheFile('../bullMoDi'))
 
-bullMoDiInferred <- vector('list', length(tipsNames))
-names(bullMoDiInferred) <- tipsNames
+data('bullMoDiInferred', package = "TreeDistData")
+if (!exists('bullMoDiInferred')) {
+  message("\n\n=== Infer trees ===\n")
+  bullMoDiInferred <- setNames(vector('list', length(tipsNames)), tipsNames)
 
-message("\n\n=== Infer trees ===\n")
-for (tipName in names(bullseyeTrees)) {
-  message('* ', tipName, ": Simulating sequences...")
-  theseTrees <- bullseyeTrees[[tipName]][seq_len(nTrees)]
-  simChar <- 2000L
-  seqs <- lapply(theseTrees, simSeq, l = simChar, type = 'USER', levels = 0:1)
-  nData <- length(seqs[[1]]) * simChar
-  inferred <- vector(mode = 'list', nTrees)
+  for (tipName in names(bullseyeTrees)) {
+    message('* ', tipName, ": Simulating sequences...")
+    theseTrees <- bullseyeTrees[[tipName]][seq_len(nTrees)]
+    simChar <- 2000L
+    seqs <- lapply(theseTrees, simSeq, l = simChar, type = 'USER', levels = 0:1)
+    nData <- length(seqs[[1]]) * simChar
+    inferred <- vector(mode = 'list', nTrees)
 
 
-  for (i in seq_along(seqs)) {
-    seq00 <- formatC(i - 1, width = 3, flag = '0')
-    cat ("", seq00)
-    FilePattern <- function (n) {
-      CacheFile(substr(tipName, 0, nchar(tipName) - 7L),
-             't-', seq00, '-k6-',
-             formatC(n, width=2, flag='0'),
-             '.tre')
-    }
-
-    if (!all(file.exists(FilePattern(subsamples)))) {
-      cat("Generating missing files: ",
-          FilePattern(subsamples)[!file.exists(FilePattern(subsamples))])
-      seqFile <- paste0(tempdir(), '\\bullMoDi-', seq00, '.tnt')
-      runRoot <- paste0(sample(letters, 8, replace = TRUE), collapse = '')
-      runFile <- paste0(runRoot, '.run', collapse='')
-      file.create(runFile)
-      write(paste("macro =;
-       xmult:hits 1 level 4 chklevel 5 rat5 drift5;
-       sect:slack 8;
-       keep 0; hold 10000;
-       piwe=6 ;xmult;tsav *", FilePattern('%1'), ";sav;tsav/;keep 0;hold 10000;
-       quit;"), runFile)
-
-      sq <- PhyDatToMatrix(seqs[[i]])
-      for (sub in subsamples) {
-        switch <- sample(seq_len(nData), nData * sub / 100L)
-        sq[switch] <- 1L - as.integer(sq[switch])
-        WriteTNTData(MatrixToPhyDat(sq), file = seqFile)
-        # Install TNT and add to the PATH environment variable before running:
-        system(paste('tnt proc', seqFile, '; ', runRoot,
-                     formatC(sub, width=2, flag='0'), ';'))
+    for (i in seq_along(seqs)) {
+      seq00 <- formatC(i - 1, width = 3, flag = '0')
+      cat ("", seq00)
+      FilePattern <- function (n) {
+        CacheFile(substr(tipName, 0, nchar(tipName) - 7L),
+               't-', seq00, '-k6-',
+               formatC(n, width=2, flag='0'),
+               '.tre')
       }
 
-      file.remove(seqFile)
-      file.remove(runFile)
-    }
+      if (!all(file.exists(FilePattern(subsamples)))) {
+        cat("Generating missing files: ",
+            FilePattern(subsamples)[!file.exists(FilePattern(subsamples))])
+        seqFile <- paste0(tempdir(), '\\bullMoDi-', seq00, '.tnt')
+        runRoot <- paste0(sample(letters, 8, replace = TRUE), collapse = '')
+        runFile <- paste0(runRoot, '.run', collapse='')
+        file.create(runFile)
+        write(paste("macro =;
+         xmult:hits 1 level 4 chklevel 5 rat5 drift5;
+         sect:slack 8;
+         keep 0; hold 10000;
+         piwe=6 ;xmult;tsav *", FilePattern('%1'), ";sav;tsav/;keep 0;hold 10000;
+         quit;"), runFile)
 
-    inferred[[i]] <-
-      lapply(formatC(subsamples, width = 2, flag='0'),
-             function (nChar) {
-               tr <- ReadTntTree(FilePattern(nChar),
-                                 relativePath = '.',
-                                 tipLabels = theseTrees[[i]]$tip.label)
-               # Return:
-               if (class(tr) == 'multiPhylo') tr[[1]] else tr
-             })
+        sq <- PhyDatToMatrix(seqs[[i]])
+        for (sub in subsamples) {
+          switch <- sample(seq_len(nData), nData * sub / 100L)
+          sq[switch] <- 1L - as.integer(sq[switch])
+          WriteTNTData(MatrixToPhyDat(sq), file = seqFile)
+          # Install TNT and add to the PATH environment variable before running:
+          system(paste('tnt proc', seqFile, '; ', runRoot,
+                       formatC(sub, width=2, flag='0'), ';'))
+        }
+
+        file.remove(seqFile)
+        file.remove(runFile)
+      }
+
+      inferred[[i]] <-
+        lapply(formatC(subsamples, width = 2, flag='0'),
+               function (nChar) {
+                 tr <- ReadTntTree(FilePattern(nChar),
+                                   relativePath = '.',
+                                   tipLabels = theseTrees[[i]]$tip.label)
+                 # Return:
+                 if (class(tr) == 'multiPhylo') tr[[1]] else tr
+               })
+    }
+    bullMoDiInferred[[tipName]] <- inferred
   }
-  bullMoDiInferred[[tipName]] <- inferred
+  usethis::use_data(bullMoDiInferred, compress = 'bzip2', overwrite = TRUE)
 }
-usethis::use_data(bullMoDiInferred, compress = 'bzip2', overwrite = TRUE)
 
 
 message("\n\n === Calculate distances ===\n")
@@ -126,12 +128,13 @@ for (tipName in tipsNames) {
 
     normInfo <- SplitwiseInfo(tr)
     cbind(
+      # The order here MUST correspond to the dimnames template below!
       pid = DifferentPhylogeneticInfo(tr, trs, normalize = TRUE),
       msid = MatchingSplitInfoDistance(tr, trs, normalize = TRUE),
       cid = ClusteringInfoDistance(tr, trs, normalize = TRUE),
+      nye = NyeSimilarity(tr, trs, similarity = FALSE, normalize = TRUE),
       qd = Quartet::QuartetDivergence(Quartet::QuartetStatus(trs, cf=tr),
                                       similarity = FALSE),
-      nye = NyeSimilarity(tr, trs, similarity = FALSE, normalize = TRUE),
 
       jnc2 = JaccardRobinsonFoulds(tr, trs, k = 2, allowConflict = FALSE,
                                   normalize = TRUE),
@@ -157,10 +160,18 @@ for (tipName in tipsNames) {
 
       rf = RobinsonFoulds(tr, trs),
       icrf = InfoRobinsonFoulds(tr, trs),
-      path = path.dist(tr, trs)
+      path = path.dist(tr, trs),
+
+      kc = KendallColijn(tr, trs),
+      es = KendallColijn(tr, trs, SplitVector)
     )
-  }, matrix(0, nrow = 10L, ncol = 21L,
-            dimnames=list(subsamples, tdMethods[-22]))
+  }, matrix(0, nrow = 10L, ncol = 25L,
+            dimnames = list(subsamples, c('pid', 'msid', 'cid', 'nye', 'qd',
+                                          'jnc2', 'jnc4', 'jco2', 'jco4',
+                                          'ms', 'mast', 'masti', 'nni_l',
+                                          'nni_L', 'nni_t', 'nni_U', 'nni_u',
+                                          'spr', 'tbr_l', 'tbr_u', 'rf',
+                                          'icrf', 'path', 'kc', 'es')))
   )
   bullMoDiScores[[tipName]] <- theseScores
 }
